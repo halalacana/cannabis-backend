2a74c3832534fe53cda065ba1fe4b45768af4614
Rafayel Karapetyan <rafa@Rafayels-MacBook-Air.local>
2025-03-01 17:54:07 -0800

    Fix

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -24,4 +24,26 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Get all menu items
+router.get("/", async (req, res) => {
+  try {
+    const menu = await MenuItem.find();
+    res.json(menu);
+  } catch (err) {
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+// Get a single menu item by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const item = await MenuItem.findById(req.params.id);
+    if (!item) return res.status(404).json({ error: "Item not found" });
+
+    res.json(item);
+  } catch (err) {
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -19,6 +22,7 @@ const orderRoutes = require("./routes/orderRoutes");
 // Use routes
 app.use("/api/menu", menuRoutes);
 app.use("/api/orders", orderRoutes);
+
 
 // Set PORT dynamically for Render
 const PORT = process.env.PORT || 5000;
